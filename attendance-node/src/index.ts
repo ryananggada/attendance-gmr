09befdabe5ef43434c3d3dae03f81ab31e4912bf
ryananggada <khoryananggada@gmail.com>
2025-12-22 11:51:59 +0700

    Debug mode to resolve CORS error 2

--- attendance-node/src/index.ts
+++ attendance-node/src/index.ts
@@ -52,10 +52,6 @@ app.get('/api', (_req, res) => {
   res.send('Hello from Typescript Express!');
 });
 
-app.listen(PORT, () => {
-  console.log(`Server listening at http://localhost:${PORT}`);
-});
-
 app.use('/api/auth', authRoute);
 app.use('/api/users', userRoute);
 app.use('/api/attendances', attendanceRoute);
@@ -67,4 +63,8 @@ app.use('/images', express.static(path.join(__dirname, '../uploads')));
 app.use((err: Error, _req: Request, res: Response, _next: NextFunction) => {
   console.error(err);
   res.status(500).json({ message: err.message || 'Internal Server Error' });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server listening at http://localhost:${PORT}`);
 });
