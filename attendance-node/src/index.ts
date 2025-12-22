18c96f6a7ede117fcbbb3b71f6dea547398dcee6
ryananggada <khoryananggada@gmail.com>
2025-12-22 10:55:58 +0700

    Add trust proxy to resolve CORS issues

--- attendance-node/src/index.ts
+++ attendance-node/src/index.ts
@@ -18,6 +18,7 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.set('json spaces', 2);
+app.set('trust proxy', 1);
 
 app.use(morgan('dev'));
 
