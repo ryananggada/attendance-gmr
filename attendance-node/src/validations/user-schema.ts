0634075ca04ec3b60907c13f1a06052f68f90afd
ryananggada <khoryananggada@gmail.com>
2025-12-29 13:16:08 +0700

    Remove confirm password validation

--- attendance-node/src/validations/user-schema.ts
+++ attendance-node/src/validations/user-schema.ts
@@ -1,21 +1,15 @@
 import { z } from 'zod';
 
-export const createUserSchema = z
-  .object({
-    username: z.string().min(1, 'Username dibutuhkan'),
-    fullName: z.string().min(1, 'Nama dibutuhkan'),
-    password: z
-      .string()
-      .min(1, 'Password dibutuhkan')
-      .min(8, 'Password harus 8 karakter atau lebih'),
-    confirmPassword: z.string().min(1, 'Confirm password dibutuhkan'),
-    departmentId: z.number('Department harus dipilih'),
-    role: z.enum(['User', 'Admin'], 'Role harus dipilih'),
-  })
-  .refine((data) => data.password === data.confirmPassword, {
-    message: 'Confirm password harus sama password',
-    path: ['confirmPassword'],
-  });
+export const createUserSchema = z.object({
+  username: z.string().min(1, 'Username dibutuhkan'),
+  fullName: z.string().min(1, 'Nama dibutuhkan'),
+  password: z
+    .string()
+    .min(1, 'Password dibutuhkan')
+    .min(8, 'Password harus 8 karakter atau lebih'),
+  departmentId: z.number('Department harus dipilih'),
+  role: z.enum(['User', 'Admin'], 'Role harus dipilih'),
+});
 
 /*
 export const updateUserSchema = z
